0cd7b6b3335cb2cf03faadf249bca865680341e7
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2023-09-28 19:06:59 +0300

    some fixes

--- Custome_auth/urls.py
+++ Custome_auth/urls.py
@@ -5,10 +5,10 @@ from .views import *
 
 urlpatterns =[
     path('login/',LoginView.as_view(),name='baselogin'),
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
     path('logout/',LogoutView.as_view(),name='baselogout'),
     path('Good-bye/',views.bye,name='Goodbye'),
-    path('',views.welcome,name = 'welcome'),
+    path('home/',views.welcome,name = 'welcome'),
     path('register/',views.register_request,name='registerpage'),
     path('form/',test_form.as_view(),name = 'oo'),
 ]
