219b3b99848e907845227df792b59168130e96dc
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2023-09-28 18:34:21 +0300

    first commit

---
+++ Custome_auth/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path 
+from django.contrib.auth.views import LoginView,LogoutView
+from . import views
+from .views import *
+
+urlpatterns =[
+    path('login/',LoginView.as_view(),name='baselogin'),
+    path('home/',views.home,name='home'),
+    path('logout/',LogoutView.as_view(),name='baselogout'),
+    path('Good-bye/',views.bye,name='Goodbye'),
+    path('',views.welcome,name = 'welcome'),
+    path('register/',views.register_request,name='registerpage'),
+    path('form/',test_form.as_view(),name = 'oo'),
+]
